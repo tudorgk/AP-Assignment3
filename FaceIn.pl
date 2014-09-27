1585ba706925f3fecf62544d224db1e0191f20a7
Tudor Dragan <me@tudordev.com>
2014-09-27 11:15:21 +0200

    working on clique

--- FaceIn.pl
+++ FaceIn.pl
@@ -11,20 +11,35 @@ g([person(susan, [reed, jen, andrzej, jessica]),
 %member function
 memb(X, [Y|T]) :- X = Y; memb(X, T).
 
-
-is_friend([person(Y,Friends)|Relations],X,Y):- 
+%is X a friend of Y?
+is_friend([person(Y,Friends)|T],X,Y):- 
 	memb(X,Friends).
 is_friend([H|T],X,Y):-
 	is_friend(T,X,Y).
 
+%seems intuitive enough
 goodfriends(G, X, Y) :-
 	is_friend(G,X,Y),
 	is_friend(G,Y,X).
 
+permutation([], []).
+permutation(List, [Element | Permutation]) :-
+	select(Element, List, Rest),
+	permutation(Rest, Permutation).	
+
+
+%bang bang cliquety clack clack
+clique(G,[Elem|L]) :-
+	goodfriends(G,X,Elem),
+	clique(G,L).
+
 :- begin_tests(friendtest).
 test(friend):-
 	g(G),
-	is_friend(G,reed,susan).
+	is_friend(G,jen,susan).
+test(goodfriends):-
+	g(G),
+	goodfriends(G,susan,jen).
 :- end_tests(friendtest).
 
 %% goodfriends(G, X, Y) :- 
