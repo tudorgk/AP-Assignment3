bc6a3294b63d1e8cdb0940e734525e720b85ee19
Tudor Dragan <me@tudordev.com>
2014-09-28 18:25:32 +0200

    not finished, but submitting anyway

--- FaceIn.pl
+++ FaceIn.pl
@@ -1,12 +1,23 @@
+% Authors : Tudor Dragan & Yoann Moise
+
 :- use_module(library(plunit)).
 
-g([person(susan, [reed, jen, andrzej, jessica]),
+g0([person(susan, [reed, jen, andrzej, jessica]),
       person(reed, [tony, jessica]),
       person(jessica, [jen]),
       person(tony, []),
       person(ken, [andrzej]),
       person(jen, [tony, susan, jessica]),
       person(andrzej, [susan, ken])]).
+
+g1([person(susan, [reed, jen, andrzej, jessica]),
+      person(reed, [tony, jessica]),
+      person(jessica, [jen,susan]),
+      person(tony, []),
+      person(ken, [andrzej]),
+      person(jen, [tony, susan, jessica]),
+      person(andrzej, [susan, ken])]).
+
 
 %member function
 memb(X, [Y|T]) :- X = Y; memb(X, T).
@@ -16,9 +27,9 @@ selec(X,[X|L],L).
 selec(X,[Y|M],[Y|N]):- selec(X,M,N).
 
 %is X a friend of Y?
-is_friend([person(Y,Friends)|T],X,Y):- 
+is_friend([person(Y,Friends)|_],X,Y):- 
 	memb(X,Friends).
-is_friend([H|T],X,Y):-
+is_friend([_|T],X,Y):-
 	is_friend(T,X,Y).
 
 %seems intuitive enough
@@ -27,7 +38,7 @@ goodfriends(G, X, Y) :-
 	is_friend(G,Y,X).
 
 build_person_list([],[]).
-build_person_list([person(Y,Friends)|T],[Y|List]) :-
+build_person_list([person(Y,_)|T],[Y|List]) :-
 	build_person_list(T,List).
 
 permutation([], []).
@@ -49,31 +60,64 @@ goodFriendWith(G, FriendOne, [FriendTwo | FriendList]):-
 	goodFriendWith(G, FriendOne, FriendList),
  	goodfriends(G, FriendOne, FriendTwo).
 
+%clique predicate
 clique(G, [FriendOne, FriendTwo]):-
  	goodfriends(G, FriendOne, FriendTwo).
 clique(G, [FriendOne | FriendList]):-
   	clique(G, FriendList),	
   	goodFriendWith(G, FriendOne, FriendList).
 
-%% clique2([person(X,XFriends),person(X,XFriends)],[]):-
-%% 	.
-%% clique2([person(X,Friends)|T], [Y|Rest]) :-
-%% 	goodfriends(G,Y,X),
-%% 	clique2(T, Rest).
-%% 	%goodfriends(G,Elem,Rest).
-%% 	%clique(G, [Elem | Rest]).
 
+%for the wannabe we should implement a DFS search and check if X
+%can reach all the other people
+%% wannabe([person(Person,Friends)], X):-
+%% wannabe([person(Person,Friends) | _],X) :-
+
+
+%% TESTING SUITE
 :- begin_tests(friendtest).
-test(friend):-
-	g(G),
+test(memb1):-
+	memb(bob, [bob, john, grace, william]).
+test(memb2,all(X=[bob,john,grace,william])):-
+	memb(X, [bob, john, grace, william]).
+test(selec1,L=[a,b,d,e]):-
+	selec(c, [a,b,c,d,e], L).
+test(selec2,L=[a,b,d,e,c]):-
+	selec(c, [a,b,c,d,e,c], L).
+test(subset1,[fail]):-
+	subset([a,b,c,d,e],[c,d,e,f,g]).
+test(subset2):-
+	subset([a,b,c,d,e],[c,d,e]).
+test(permutation1):-
+	permutation([a,b,c,d], L).
+test(is_friend1):-
+	g0(G),
 	is_friend(G,jen,susan).
-test(goodfriends):-
-	g(G),
+test(is_friend2,[fail]):-
+	g0(G),
+	is_friend(G,tony,susan).
+test(goodfriends_true1):-
+	g0(G),
 	goodfriends(G,susan,jen).
+test(goodfriends_false1,[fail]):-
+	g0(G),
+	goodfriends(G,susan,tony).
+test(goodfriends_false2,[fail]):-
+	g1(G),
+	goodfriends(G,tony,jessica).
+test(clique_true1):-
+	g0(G),
+	clique(G,[susan,jen]).
+test(clique_true2):-
+	g0(G),
+	clique(G,[susan,andrzej]).
+test(clique_true3):-
+	g1(G),
+	clique(G,[jessica,jen,susan]).
+test(clique_false1,[fail]):-
+	g1(G),
+	clique(G,[ken,susan]).
 :- end_tests(friendtest).
-
-%% goodfriends(G, X, Y) :- 
-%% 	graph(G),
 
 %% comparing all elemts to head
 small([M|T],X):- small1(T, M, X).
@@ -81,9 +125,11 @@ small([M|T],X):- small1(T, M, X).
 small1([H|_], M ,H):- H =< M.
 small1([_|T], M, X):- small1(T, M, X).
 
+%for debugging
 print_all([]).
 print_all([X|Rest]) :- write(X), nl, print_all(Rest).
 
+%implemented map, but I don't use it
 maplist(_C_2, [], []).
 maplist( C_2, [X|Xs], [Y|Ys]) :-
    call(C_2, X, Y),
--- README.md
+++ README.md
@@ -1,4 +1,33 @@
 AP-Assignment3
 ==============
 
-Analysing FaceIn - This assignment is about analysing social relations on the social network FaceIn.
+% Authors : Tudor Dragan & Yoann Moise
+
+Assignment 3 : Analysing FaceIn
+28/09/2014
+
+Subject : This assignment is about analysing social relations on the social network FaceIn using Prolog.
+
+Remarks : No error/warning during the compilation.
+
+
+To better understand the subject, we started to reflect on how we could know if one element is a friend with another one.
+The best thing was to use the function member that we did .
+
+Having is_friend function, the goodfriends function was intuitive enough. It needs that a first person is a friend with a second person and that the second person is friend with the first person.
+
+We thought also that the functions selec (equivalent of select/3), subset and permutation would be interesting to build for further use.
+
+We created the build_person_list function thinking that it can be good to use it for the clique function. Finally we didn't use it, but it may be good to use it for other functions.
+
+The function clique is not correctly implemented.
+It will produce the correct result if the parameters are known.
+If not, it will work but only for pairs (two people).
+If there are more people concerned, there might be an infinite loop.
+We didn't find how to resolve this (yet).
+This function uses memb, selec and subset.
+
+Testing:
+	At the end of the file FaceIn.pl, we used fifteen tests functions to test all the functions we created.
+	Depending of the tests, it could return either a boolean result (true/false), a list or an atom.
+	Testing have been done one function with known and unknown parameters in all cases except clique which would lead to infinite recursion.
